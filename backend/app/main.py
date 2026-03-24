50efdb1d435f027a16b9faec8eb0e02058fd1a84
mariemgs <mariemhes@gmail.com>
2026-03-24 11:16:33 +0100

    test: trigger AI analyzer

--- backend/app/main.py
+++ backend/app/main.py
@@ -36,3 +36,4 @@ if settings.all_cors_origins:
 Instrumentator().instrument(app).expose(app)
 
 app.include_router(api_router, prefix=settings.API_V1_STR)this will breakgit push origin main!
+this will breakgit push origin main!
